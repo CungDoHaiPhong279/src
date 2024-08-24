ed82f4281ac88377f9d87c14d43ed1a9e81dd1f9
Cung Do Hai Phong <23010341@st.phenikaa-uni.edu.vn>
2024-08-25 01:28:14 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+   public static void main(String[] args) {
+       System.out.println("Hello My Name Is Vinh");
+       System.out.println("Toi 18 tuoi");
+       System.out.println("Tamlearning Java");
+       System.out.println(3);
+       System.out.println(359);
+       System.out.println(3*5); //Phep nhan
+       String firstName = "John ";
+String lastName = "Doe";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+String studentName = "John Doe";
+int studentID = 15;
+int studentAge = 23;
+float studentFee = 75.25f;
+char studentGrade = 'B';
+
+// Print variables
+System.out.println("Student name: " + studentName);
+System.out.println("Student id: " + studentID);
+System.out.println("Student age: " + studentAge);
+System.out.println("Student fee: " + studentFee);
+System.out.println("Student grade: " + studentGrade);
+int length = 4;
+int width = 6;
+int area;
+
+// Calculate the area of a rectangle
+area = length * width;
+
+// Print variables
+System.out.println("Length is: " + length);
+System.out.println("Width is: " + width);
+System.out.println("Area of the rectangle is: " + area);
+   }
+}
